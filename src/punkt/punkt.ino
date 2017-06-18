a50cdfde491852aa302f2847d2ca7fac3d32d90c
Andy O'Neill <aoneill@measinc.com>
2017-06-18 15:18:41 -0400

    Adding title screen and instructions. Adding initial HEX file.

--- src/punkt/punkt.ino
+++ src/punkt/punkt.ino
@@ -10,6 +10,11 @@
 
 #define MODE_PLAYING 0
 #define MODE_GAME_OVER 1
+#define MODE_TITLE_SCREEN 2
+#define MODE_MAIN_MENU 3
+#define MODE_INSTRUCTIONS 4
+
+#define TITLE_X ((LCDWIDTH - (5 * 6)) >> 1)
 
 Gamebuino gb;
 // Represents the player
@@ -22,8 +27,10 @@ Target target;
 byte score;
 byte highScore;
 byte mode;
+unsigned long referenceTime;
 
 extern const byte font3x5[];
+extern const byte font5x7[];
 
 void setup() {
   gb.begin();
@@ -42,6 +49,16 @@ void loop() {
       case MODE_GAME_OVER:
         doGameOver();
         break;
+      case MODE_TITLE_SCREEN:
+        referenceTime = gb.frameCount;
+        mode = MODE_MAIN_MENU;
+        break;
+      case MODE_MAIN_MENU:
+        doMainMenu();
+        break;
+      case MODE_INSTRUCTIONS:
+        doInstructions();
+        break;
     }    
   }
 }
@@ -50,8 +67,7 @@ void reset() {
   gb.titleScreen(F("punkt"));
   gb.setFrameRate(30);
   gb.battery.show = false;
-
-  initializeGame();
+  mode = MODE_TITLE_SCREEN;
 }
 
 void initializeGame() {
@@ -60,22 +76,32 @@ void initializeGame() {
   player = Player();
   target = Target();
   mode = MODE_PLAYING;
+  gb.sound.playOK();
 }
 
 void doPlaying() {
   player.update(gb);
-  
+
+  // Check if we've hit an enemy
   for (byte i = 0; i < enemyCount; i++) 
   {
     enemies[i].update(gb.frameCount);
-    if (enemies[i].isHit(player.getX(), player.getY(), 4)) mode = MODE_GAME_OVER;
+    if (enemies[i].isHit(player.getX(), player.getY(), 4)) {
+      gb.sound.playCancel();
+      mode = MODE_GAME_OVER;
+    }
   }
 
+  // Check if we've captured a target
   if (target.isHit(player.getX(), player.getY(), 4)) {
-    target.moveToNewLocation();
+    gb.sound.playOK();
+    do {
+      target.moveToNewLocation();
+    } while (target.isHit(player.getX(), player.getY(), 4));
     if (enemyCount < MAX_ENEMIES) {
       enemies[enemyCount++].spawn(gb.frameCount);
     }
+    // Update score
     if (score < 100) score++;
     if (score > highScore) highScore = score;
   }
@@ -95,7 +121,97 @@ void doGameOver() {
   gb.display.cursorY = ((BOARD_HEIGHT - 7) >> 1) + 1;
   gb.display.print("Game Over");
 
-  if (gb.buttons.pressed(BTN_A) || gb.buttons.pressed(BTN_B)) initializeGame();
+  if (gb.buttons.pressed(BTN_A) || gb.buttons.pressed(BTN_B)) {
+    initializeGame();
+  }
+}
+
+void doMainMenu() {
+  if (gb.buttons.pressed(BTN_A)) {
+    initializeGame();
+    return;
+  }
+  else if (gb.buttons.pressed(BTN_B)) {
+    referenceTime = gb.frameCount;
+    mode = MODE_INSTRUCTIONS;
+    // So instructions always look the same.
+    randomSeed(1);
+    target.moveToNewLocation();
+    enemies[0].spawn(gb.frameCount);
+    enemies[1].spawn(gb.frameCount);
+    player = Player();
+    return;
+  }
+  
+  unsigned long t = gb.frameCount - referenceTime;
+  
+  gb.display.setColor(BLACK);
+  gb.display.setFont(font5x7);
+  gb.display.cursorY = t < 22 << 1 ? -7 + (t >> 1) : 15;
+  gb.display.cursorX = TITLE_X;
+  gb.display.print("punkt");
+
+  if (t > 22 << 1) {
+    gb.display.setFont(font3x5);
+    gb.display.cursorY = 25;
+    gb.display.cursorX = TITLE_X - 5;
+    gb.display.print("Start");
+    gb.display.cursorY = 33;
+    gb.display.cursorX = TITLE_X - 5;
+    gb.display.print("Instructions");
+
+    if (t & 0x08)
+    {
+      gb.display.setFont(font5x7);
+      gb.display.cursorY = 24;
+      gb.display.cursorX = TITLE_X - 11;
+      gb.display.print('\25');
+      gb.display.cursorY = 32;
+      gb.display.cursorX = TITLE_X - 11;
+      gb.display.print('\26');
+    }
+  }
+}
+
+void doInstructions() {
+  if (gb.buttons.pressed(BTN_A) || gb.buttons.pressed(BTN_B)) {
+    referenceTime = gb.frameCount;
+    mode = MODE_MAIN_MENU;
+    gb.pickRandomSeed();
+    return;
+  }
+  
+  unsigned long t = gb.frameCount - referenceTime;
+
+  gb.display.setColor(BLACK);
+  gb.display.setFont(font3x5);
+  
+  if (t < 100) {
+    gb.display.cursorX = 31;
+    gb.display.cursorY = 14;
+    gb.display.print("you");
+    player.draw(gb);
+  }
+  else if (t < 250) {
+    gb.display.cursorX = 10;
+    gb.display.cursorY = 14;
+    gb.display.print("collect these");
+    target.draw(gb); 
+  }
+  else if (t < 400) {
+    gb.display.cursorX = 14;
+    gb.display.cursorY = 14;
+    gb.display.print("avoid these");
+    enemies[0].update(gb.frameCount);
+    enemies[1].update(gb.frameCount);
+    enemies[0].draw(gb);
+    enemies[1].draw(gb);
+  }
+  else { 
+    referenceTime = gb.frameCount;
+    mode = MODE_MAIN_MENU;
+    gb.pickRandomSeed();
+  }
 }
 
 void drawBoard() {
